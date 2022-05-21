dfb46a8c701d408bb034b26a0b9c1b0e611509a3
Tayyab <aolakh4g@gmail.com>
2022-05-21 05:28:53 +0500

    Login module

--- config/db.js
+++ config/db.js
@@ -1,12 +1,35 @@
 import mysql from "mysql";
 
-var conn = mysql.createPool({
-  connectionLimit: 10,
-  host: "localhost",
-  user: "root",
-  password: "tiger123",
-  database: "nla_database",
-});
+// var conn = mysql.createPool({
+//   connectionLimit: 10,
+//   host: "localhost",
+//   user: "root",
+//   password: "tiger123",
+//   database: "nla_database",
+// });
+
+let connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var conn = mysql.createPool({
+    connectionLimit: 10,
+    host: "localhost",
+    user: "root",
+    password: "tiger123",
+    database: "nla_database",
+  });
+}
+
+// var conn = mysql.createPool({
+//   connectionLimit: 10,
+//   host: "ebh2y8tqym512wqs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+//   user: "v0c2ossw46w6oj38",
+//   password: "wqg8kch7gptia1u2",
+//   port: "3306",
+//   database: "	e4fum3snt275n7lx",
+// });
 
 conn.query("SELECT 1 + 3 AS solution", function (error, results, fields) {
   if (error) {

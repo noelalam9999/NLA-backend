3db4dd5be0d1b5c80ec3094ad8acfd13b7042442
Tayyab <aolakh4g@gmail.com>
2022-05-21 05:56:55 +0500

    Login module

--- server.js
+++ server.js
@@ -4,6 +4,6 @@ import app from "./app.js";
 // -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-==-=-=-=-=-=--=-=-=-=-=-=-=-=-=-=-=-=-=-=-=
 
 // PORT
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 const server = http.createServer(app);
 server.listen(port, () => console.log(`Server is running on ${port}..`));
